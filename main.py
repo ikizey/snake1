0176f77e33251da2a67fa8a8feb4a52577cd1e78
ikizey <ikizey@gmail.com>
2018-05-01 00:27:54 +0300

    same functionality. but tottaly rewritten using Sprites

--- main.py
+++ main.py
@@ -1,118 +1,134 @@
-"""
- Pygame base template for opening a window
-
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
-
- Explanation video: http://youtu.be/vRB_983kUMc
-"""
-
 import pygame
 import random
 from colour import COLOR_NAME_TO_RGB as C
 
-pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+BASE_PIXEL_WIDTH = 20
+
+pygame.init()
+
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Snake")
 font = pygame.font.SysFont(None, 20, False, False)
+game_speed_text = font.render('Game Speed: 1', True, C['black'])
 
-# Some Coordinates
-TOP_LEFT = [0, 0]
-TOP_RIGHT = [WINDOW_WIDTH, 0]
-BOTTOM_LEFT = [0, WINDOW_HEIGHT]
-BOTTOM_RIGHT = [WINDOW_WIDTH, WINDOW_HEIGHT]
+done = False
+clock = pygame.time.Clock()
 
-# Game speed
 game_speed = 12
 time_tracker = 0
 
-# Border Color
-BORDER_COLOR = C['white']
 
-# Snake code
-snake_pixel_size = 20
-snake_coords = [[400, 300]]
-snake_speeds = ((0, snake_pixel_size), (snake_pixel_size, 0), (0, -snake_pixel_size), (-snake_pixel_size, 0))
-snake_speed_x, snake_speed_y = snake_speeds[0]
-SNAKE_HEAD_COLOR = C['limegreen']
-SNAKE_BODY_COLOR = C['darkgreen']
-# cherry
-cherry_coord_x, cherry_coord_y = 400, 320
+class Pixel(pygame.sprite.Sprite):
+    def __init__(self, color, coords):
+        super().__init__()
+        self.image = pygame.Surface([BASE_PIXEL_WIDTH, BASE_PIXEL_WIDTH])
+        self.set_color(color)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = coords
 
-
-def cherry_random_coordinates():
-    while True:
-        new_x = random.randrange(snake_pixel_size * 2, WINDOW_WIDTH - snake_pixel_size * 2, snake_pixel_size)
-        new_y = random.randrange(snake_pixel_size * 2, WINDOW_HEIGHT - snake_pixel_size * 2, snake_pixel_size)
-        if not ([new_x, new_y] in snake_coords):
-            break
-    return new_x, new_y
+    def set_color(self, color):
+        self.image.fill(color)
 
 
-clock = pygame.time.Clock()
-done = False
+class Cherry(Pixel):
+    def __init__(self, coords):
+        super().__init__(C['red'], coords)
+
+    # Infinite loop possible if snake occupies almost all screen area
+    def new_pos(self, forbidden_coords):
+        while True:
+            new_x = random.randrange(BASE_PIXEL_WIDTH * 2, WINDOW_WIDTH - BASE_PIXEL_WIDTH * 2, BASE_PIXEL_WIDTH)
+            new_y = random.randrange(BASE_PIXEL_WIDTH * 2, WINDOW_HEIGHT - BASE_PIXEL_WIDTH * 2, BASE_PIXEL_WIDTH)
+            if not ([new_x, new_y] in forbidden_coords):
+                break
+        self.rect.x = new_x
+        self.rect.y = new_y
+
+
+class Snake:
+    def __init__(self):
+        self.pixels = [Pixel(C['darkgreen'], [400, 300])]
+        self.DIRECTIONS = ((0, BASE_PIXEL_WIDTH), (BASE_PIXEL_WIDTH, 0), (0, -BASE_PIXEL_WIDTH), (-BASE_PIXEL_WIDTH, 0))
+        self.speed = self.DIRECTIONS[0]
+
+    def move(self):
+        if len(self.pixels) > 1:
+            self.pixels.pop().kill()
+            self.pixels.insert(0, Pixel(C['limegreen'], self.get_head_pos()))
+        self.pixels[0].rect.x += self.speed[0]
+        self.pixels[0].rect.y += self.speed[1]
+        self._reclor()
+
+    def turn(self, direction):
+        self.speed = self.DIRECTIONS[self.DIRECTIONS.index(self.speed) - direction]
+
+    def grow(self):
+        self.pixels.insert(0, Pixel(C['limegreen'], self.get_head_pos()))
+
+    def get_head_pos(self):
+        return [self.pixels[0].rect.x, self.pixels[0].rect.y]
+
+    def on(self, surface):
+        return surface.get_rect().contains(self.pixels[0].rect)
+
+    def _reclor(self):
+        for pix in self.pixels:
+            if pix is not self.pixels[0]:
+                pix.set_color(C['darkgreen'])
+
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill(C['black'])
+pygame.draw.rect(background, C['white'], background.get_rect(), BASE_PIXEL_WIDTH * 2)
+
+snake = Snake()
+cherry = Cherry([400, 330])
+snake_sprites = pygame.sprite.RenderPlain(snake.pixels)
+cherry_sprites = pygame.sprite.RenderPlain(cherry)
+cherry_eaten = []
+snake_body_sprites = []
+
+# ---Main loop
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snake_speed_x, snake_speed_y = snake_speeds[snake_speeds.index((snake_speed_x, snake_speed_y)) - 3]
+                snake.turn(3)
             if event.key == pygame.K_RIGHT:
-                snake_speed_x, snake_speed_y = snake_speeds[snake_speeds.index((snake_speed_x, snake_speed_y)) - 1]
+                snake.turn(1)
 
-    # Lose
-    # --Hit a wall
-    if snake_coords[0][0] < 0 + snake_pixel_size or snake_coords[0][0] > WINDOW_WIDTH - snake_pixel_size:
-        done = True
-    if snake_coords[0][1] < 0 + snake_pixel_size or snake_coords[0][1] > WINDOW_HEIGHT - snake_pixel_size:
-        done = True
-    # --Eat itself
-    if snake_coords[0] in snake_coords[2:]:
-        done = True
+    snake_bitten = pygame.sprite.spritecollide(snake.pixels[0], snake_body_sprites, False)
+    if not snake_bitten and snake.on(screen):
 
-    # Snake Movement + Game Speed
-    time_tracker += 1
-    # Increase game speed every 20? secs(?)
-    if time_tracker == 20 * 60:
-        time_tracker = 0
+        cherry_eaten = pygame.sprite.spritecollide(cherry, snake_sprites, False)
+        if cherry_eaten:
+            cherry_eaten = []
+            cherry.new_pos(snake.get_head_pos())
+            snake.grow()
+
         if game_speed > 1:
-            game_speed += -1
-    text = font.render(' '.join(['Game Speed:', str(13 - game_speed)]), True, C['black'])
-    #
-    if time_tracker % game_speed == 0:
-        if len(snake_coords) > 1:
-            snake_coords.pop()
-            snake_coords.insert(0, snake_coords[0][:])
-        snake_coords[0][0], snake_coords[0][1] = snake_coords[0][0] + snake_speed_x, snake_coords[0][1] + snake_speed_y
+            time_tracker += 1
+            if time_tracker == 20 * 60:
+                time_tracker = 0
+                game_speed -= 1
+                game_speed_text = font.render(' '.join(['Game Speed:', str(13 - game_speed)]), True, C['black'])
+        if time_tracker % game_speed == 0:
+            screen.blit(background, screen.get_rect())
+            snake.move()
+            snake_sprites = pygame.sprite.RenderPlain(snake.pixels)
+            snake_body_sprites = pygame.sprite.RenderPlain(snake.pixels[1:])
+            screen.blit(game_speed_text, screen.get_rect())
+            cherry_sprites.draw(screen)
+            snake_sprites.draw(screen)
+            pygame.display.flip()
 
-    # cherry eaten
-    if (snake_coords[0][0], snake_coords[0][1]) == (cherry_coord_x, cherry_coord_y):
-        snake_coords.append(snake_coords[-1][:])
-        cherry_coord_x, cherry_coord_y = cherry_random_coordinates()
-    screen.fill(C['black'])
-    # Draw Borders
-    pygame.draw.line(screen, BORDER_COLOR, TOP_LEFT, BOTTOM_LEFT, snake_pixel_size * 2)
-    pygame.draw.line(screen, BORDER_COLOR, TOP_LEFT, TOP_RIGHT, snake_pixel_size * 2)
-    pygame.draw.line(screen, BORDER_COLOR, BOTTOM_LEFT, BOTTOM_RIGHT, snake_pixel_size * 2)
-    pygame.draw.line(screen, BORDER_COLOR, TOP_RIGHT, BOTTOM_RIGHT, snake_pixel_size * 2)
-    # Draw Snake
-    pygame.draw.rect(screen, C['red'], [cherry_coord_x, cherry_coord_y, snake_pixel_size, snake_pixel_size])
-    for i in range(len(snake_coords)):
-        if i == 0:
-            pygame.draw.rect(screen, SNAKE_HEAD_COLOR,
-                             [snake_coords[i][0], snake_coords[i][1], snake_pixel_size, snake_pixel_size])
-        else:
-            pygame.draw.rect(screen, SNAKE_BODY_COLOR,
-                             [snake_coords[i][0], snake_coords[i][1], snake_pixel_size, snake_pixel_size])
-    screen.blit(text, TOP_LEFT)
-
-    pygame.display.flip()
-
-    clock.tick(60)
+        clock.tick(60)
+    else:
+        done = True
 
 pygame.quit()
